7b31a5879bf9a819aa59628e462b6460a1102527
Ricky Collins <richardcollins52317@gmail.com>
2018-07-19 09:58:26 -0400

    Styling is done for pages 1, 2, and 3, all information is being passed to each page succefully, only need to syle the last page.

--- app.rb
+++ app.rb
@@ -33,3 +33,28 @@ get '/lucky' do
   zipcode = params[:zipcode]
   erb :lucky, :layout => :layout, :locals => {first_name: first_name, last_name: last_name, mail_info: mail_info, state: state, zipcode: zipcode}
 end
+
+post '/fav_nums' do
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  mail_info = params[:mail_info]
+  state = params[:state]
+  zipcode = params[:zipcode]
+  fav_num1 = params[:fav_num1.to_s]
+  fav_num2 = params[:fav_num2.to_s]
+  fav_num3 = params[:fav_num3.to_s]
+  redirect '/display?first_name=' + first_name + '&last_name=' + last_name + '&mail_info=' + mail_info + '&state=' + state + '&zipcode=' + zipcode + '&fav_num1=' + fav_num1 + '&fav_num2=' + fav_num2 + '&fav_num3=' + fav_num3
+end
+
+get '/display' do
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  mail_info = params[:mail_info]
+  state = params[:state]
+  zipcode = params[:zipcode]
+  fav_num1 = params[:fav_num1]
+  fav_num2 = params[:fav_num2]
+  fav_num3 = params[:fav_num3]
+  sum = fav_num1.to_i + fav_num2.to_i + fav_num3.to_i
+  erb :display, :layout => :layout, :locals => {first_name: first_name, last_name: last_name, mail_info: mail_info, state: state, zipcode: zipcode, sum: sum}
+end
